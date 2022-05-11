bde1faf7e8c701945fd5ab642b93ecba49715757
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2022-05-11 20:01:15 +0000

    v1.0.0, Wed 11 May 2022 08:01:15 PM UTC

---
+++ docs/index.a689afeb.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(e=>{const t=document.createElement("div");t.style.color="white",t.textContent="Hello, world!",document.body.appendChild(t)}));
+//# sourceMappingURL=index.a689afeb.js.map
