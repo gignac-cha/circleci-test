ec67be4db5c4b9d779de9bb9f58e54d69d6ed0f5
Jin-Hyeok, Cha <38489680+gignac-cha@users.noreply.github.com>
2022-05-12 04:52:51 +0900

    fix code

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 window.addEventListener('load', (ev: Event) => {
   const div: HTMLDivElement = document.createElement('div');
+  div.style.color = 'white';
   div.textContent = 'Hello, world!';
   document.body.appendChild(div);
 });
